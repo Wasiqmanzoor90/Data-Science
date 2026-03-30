e55c6b4637fb7036c6281dc356b4ee414228b5a0
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-03-30 17:07:29 +0530

    b5

--- B3/index2.py
+++ B3/index2.py
@@ -1,2 +1,39 @@
-def add(a,b):
-    return print(a+b)
+# def add(a,b):
+#     return print(a+b)
+
+
+
+
+
+
+#file  handling is process when we create, read and updata a particular file
+#r stand for read
+#w stand for write
+#a stand for append
+
+
+#we have created a file
+# file = open("File.txt",'w')
+# file.write("Hello im from haba kadal")
+# file.close()
+
+
+# file = open("File.txt",'r')
+# data = file.read()
+# print(data)
+# file.close()
+
+
+
+
+# file = open("File.txt",'a')
+# file.write(" and my name is Reeba")
+# file.close()
+
+
+
+
+
+file = open('File.txt','w')
+file.write("Jasira")
+file.close()
