bf0158583eb7b02c2a6bb7bf9eb18c6eb2651078
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-03-31 17:13:09 +0530

    k3

--- B3/index2.py
+++ B3/index2.py
@@ -14,7 +14,7 @@
 
 #we have created a file
 # file = open("File.txt",'w')
-# file.write("Hello im from haba kadal")
+# file.write("Hello im from bemina")
 # file.close()
 
 
@@ -38,6 +38,24 @@
 # file.write("Jasira")
 # file.close()
 
-
-
+#its new way that was created by python in this the file automatically get closed
+# with open('File.txt','r') as f:
+#     data = f.read()
+#     print(data)
     
+    
+# with open('ayman.txt','w') as f:
+#     f.write("Hello world!")
+
+
+# with open("ayman.txt",'a') as f:
+#     f.write(" and my name is syed aiman")
+
+
+with open(r"C:\Users\Dell\Desktop\jasira.txt",'w') as f:
+    f.write("My name is jasira")
+    
+    
+with open(r"C:\Users\Dell\Desktop\exam.txt",'r') as f:
+    data = f.read()
+    print(data)
