a37218a692cde09c14d295153d6dafe5a3551beb
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-05-06 16:16:26 +0530

    l4

--- B5/index.py
+++ B5/index.py
@@ -1,2 +1,61 @@
-a = 12
-print(a)
+
+
+#Arthmatic operations
+# a = 12
+# b=10
+# c = a+b
+# print(c)
+
+# d = 23
+# e = 3
+# print(d-c)
+
+
+# f = 2
+# g = 10
+# print(f*g)
+
+# h = 10
+# i = 2
+# print(h/i)
+
+
+# k = 10
+# j = 2
+# print(k%j)
+
+# z = 5
+# print(z**3)
+
+# #exponent
+# a = 10
+# b=3
+# print(a/b)
+
+# #floor division
+# c = 10
+# d = 3
+# print(c//d)
+
+
+#comparison operator
+
+# a = 12
+# b = 14
+# print(a ==b)
+
+# c = 23
+# d = 34
+# print(c!=d)
+
+# g = 12
+# print(g>10)
+
+# h = 22
+# print(h<20)
+
+age = 18
+print(age>=18)
+
+nage = 12
+print(nage<=12)
