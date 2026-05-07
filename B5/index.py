20138b8087ff496fc0ded980734a2433106a40f6
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-05-07 16:12:59 +0530

    k5

--- B5/index.py
+++ B5/index.py
@@ -54,8 +54,88 @@
 # h = 22
 # print(h<20)
 
-age = 18
-print(age>=18)
+# age = 18
+# print(age>=18)
 
-nage = 12
-print(nage<=12)
+# nage = 12
+# print(nage<=12)
+
+
+#assignment operator
+# a = 19
+
+# b =12
+# # b = b+3
+# b+=8
+# print(b)
+
+# c = 13
+# c-=3
+# print(c)
+
+# d = 3
+# d*=4
+# print(d)
+
+# e = 10
+# e/=2
+# print(e)
+
+# f =10
+# f%=2
+# print(f)
+
+
+# name = input("Enter your name:-")
+# age = input("Enter your age:-")
+# print(name)
+# print(age)
+
+
+
+
+#Data type tells us which type of data a variable holds or we can say store
+#int-- int holds numerical values
+#string-- string holds textual dataand everthing that is between inside the quotes or treated as string
+#float -- float holds decimal value
+#bool -- bool holds true or flase value
+
+
+
+# a = 29
+# print(type(a))
+
+# b = '10'
+# print(type(b))
+
+# c = 10.789
+# print(type(c))
+
+
+# x = 12
+# y = 13
+# print(x==y)
+
+# z=True
+# print(type(z))
+
+
+
+# a = 10
+# print(a)
+
+# b = 'wasiq'
+# print(b)
+
+# c = 'junaid'
+# print(c)
+
+
+
+#list means when we store multiple items/values in single variable,its ordered,it's indexed,it's changeble(mutable)
+#index is numerical location of a item presented in the list
+lt = ['wasiq',12,90,'ehsaan','wareed',10.89,True]
+lt[3]='zeeshan'  #here we update the list
+lt.append('Taha') #here we simply add
+lt.insert(1,'Zamin') #here we add value by index 
+print(lt[1])
