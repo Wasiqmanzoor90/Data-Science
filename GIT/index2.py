3e77e125e970df06a5d4598bcd1dfd32ea2f1741
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-06-29 12:31:38 +0530

    k5

--- GIT/index2.py
+++ GIT/index2.py
@@ -15,24 +15,64 @@
 
 
 
-class calculator:
-    def __init__(self):
-        print("Welcome to my calculator")
+# class calculator:
+#     def __init__(self):
+#         print("Welcome to my calculator")
     
-    def add(self,x,y):
-        print(x+y)
+#     def add(self,x,y):
+#         print(x+y)
         
         
-    def Sub(self,x,y):
-        print(x-y)
+#     def Sub(self,x,y):
+#         print(x-y)
         
         
-    def Mul(self,x,y):
-        print(x*y)
+#     def Mul(self,x,y):
+#         print(x*y)
         
         
-    def divide(self,x,y):
-        print(x/y)
+#     def divide(self,x,y):
+#         print(x/y)
         
         
-cal = calculator()
+# cal = calculator()
+
+
+#file handling is the process of reading and writing data to a file. In python, there are two types of files text files and binary files. Text files are human-readable files that contain plain text, while binary files contain data in a format that is not human-readable.
+
+
+#w,a,x,r
+#W stands for creating a file
+# file= open("zamin.txt","w")
+# file.write("Hello i am from nishat and i am studyng in ils")
+# file.close()
+
+
+#r stands for rading a particular file
+# file = open("zamin.txt",'r')
+# data = file.read()
+# print(data)
+
+
+#a here means append
+# file = open('zamin.txt','a')
+# file.write(" And  i am also in islamia collage!")
+# file.close()
+
+# file = open("zamin.txt",'r')
+# d = file.read()
+# print(d)
+
+# file = open("zamin.txt",'w')
+# file.write(" And my friend is wareed")
+# file.close()
+
+
+
+# file = open(r"C:\Users\Dell\Desktop\wasiq.txt",'r')
+# p = file.read()
+# print(p)
+
+# file = open("C:\Users\Dell\Downloads\wareed.txt",'w')
+# file.write("Hello i am soura")
+# file.close()
