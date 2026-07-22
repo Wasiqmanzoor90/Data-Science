4f928ae89a6250c22887f31da1f5cdfbbe6b515d
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-07-22 15:04:39 +0530

    l8

---
+++ Web Scraping/index.py
@@ -0,0 +1,119 @@
+#Object Oriented Programing Language OOPS
+#Ensapsulation
+#Polymorphism
+#Inheritence
+#Abstraction
+
+
+
+class Ils:
+    def Hello():
+        print("Hello from Ils")
+    
+    def Bye():
+        print("Will meet you soon")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# i = Ils()
+# i.Bye()
+
+
+
+# class Cat:
+#     def speak(self):
+#         print("Meow Meow")
+        
+
+# class Dog:
+#     def speak(self):
+#         print("Bow Bow")
+        
+        
+
+
+# d = Dog()
+# c = Cat()
+
+# d.speak()
+# c.speak()
+
+
+
+# class Animal:
+#     def Tiger(self):
+#         print("Tiger is king")
+        
+# class Speak(Animal):
+#     def Comunicate(l):
+       
+#         print("Animal comunicate with each other with their language")
+        
+        
+
+
+# s = Speak()
+# s.Comunicate()
+# s.Tiger()
+
+
+
+
+
+# class Ils:
+    
+#     def __init__(self):
+#         self.feesD()
+#     def train(self):
+#         print("Wasiq is trainer")
+    
+#     def marketing(self):
+#         print("umer is marketing")
+    
+#     def cordination(self):
+#         print("Muskan is cordinator")
+        
+    
+#     def feesD(self):
+#         print("Maryam has balance of 20000")
+        
+
+# i = Ils()
+# i.train()
+# i.marketing()
+# i.cordination()
+
+
+
+
+# #
+# import time
+# def task():
+#     print("Task started")
+#     time.sleep(2)
+#     print("Task finished")
+#     time.sleep(1)
+    
+    
+# task()
+# task()
+# task()
+
+
+class Person:
+    def det(self,name,age):
+        print(name)
+        print(age)
+        
+        
+p = Person()
+name = 'Khan'
+age = 21
+del p.self.age()
+p.det(name,age)
