ca676d04219b8c637aec54f4b5171a02bd6afc44
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-08-01 12:42:30 +0530

    l3

---
+++ B6/index.py
@@ -0,0 +1,71 @@
+#coments
+
+
+
+#exponent
+# a = 3
+# print(a*a*a)
+
+
+# #flooer division
+# x = 9
+# y = 2
+# print(x//y)
+
+
+#Relational operator
+
+# a = 12
+# b =12
+# print(a==b)
+
+
+# a = 12
+# b = 12
+# print(a!=b)
+
+
+# a = 12
+# b = 11
+# print(a<b)
+
+
+# age = 18
+# print(age>=18)
+
+# age = 12
+# print(age<=12)
+
+
+#Assigment Operator
+# a = 20
+# a = a+5
+# # a+=5
+# print(a)
+
+
+#Data types  tellls which type of data a particulat variable holds
+#int-- it holds proper numerical value eg 12
+#float --it holds point values eg 12.56
+#string -- it holds textual data eg 'wasiq'
+#boolean -- it holds tru/false values
+
+
+
+# a = "wasiq"
+# print(type(a))
+
+
+# a = 10
+# print(type(a))
+
+
+# b  = 10.6
+# print(type(b))
+
+# c =True
+# print(type(c))
+
+
+a = '10.87'
+print(a)
