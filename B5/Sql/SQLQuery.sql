64ea41ddbe1137edd3ddd4d05a59af458af0aebf
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-08-24 12:53:09 +0530

    l4

---
+++ B5/Sql/SQLQuery.sql
@@ -0,0 +1,44 @@
+
+--This is single line comment
+
+--Multi line comment
+/*
+dsgcksckvjsd
+dnksxbncdslhnc
+sb ksdbjbsd
+bc dsjkblksbj
+sbsn
+ascnsx
+asn*/
+
+--Data Type tells us ehich type of value a variable holds
+/*
+int --- stores numerical data
+char -- stores textual data  fixed length
+varchar -- stores textual data variable length
+boolean  -- stores true false
+--Date   -- stores date
+*/
+
+
+/*
+DDL Data defiantion language 
+DML Data manipulation language
+TCL  Transaction control language
+*/
+
+
+Create database mydb1
+
+drop database mydb1
+use mydb1
+
+
+--Create 
+create table student
+(
+Roll_no int,
+Name Varchar(20),
+class varchar(10)
+)
+
