a8e99ef9c410caef9b33661a4d892a0cafa91b63
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-08-25 12:42:10 +0530

    l5

--- B5/Sql/SQLQuery.sql
+++ B5/Sql/SQLQuery.sql
@@ -42,3 +42,53 @@ Name Varchar(20),
 class varchar(10)
 )
 
+
+
+
+
+use mydb1
+
+/*DDL COMANDS
+Create   -- creates table
+Alter    -- it chages structure of table 
+Turncate   --- It deletes data inside a table 
+drop  -- it deletes whole table as well as data
+*/
+
+alter table student
+add section varchar(30)
+
+
+truncate table student  --deletes data inside table but not table 
+drop table student  -- it delets data as well as table
+select * from student
+
+
+
+/* DML Data manipulation language
+Select --- it fetches data from database
+insert --it adds data in particular table
+update -- it updates existing data in table
+delete  -- it delets particular row from table 
+*/
+
+
+
+select * from student
+
+insert into student(roll_no,name,class,section)
+values(2,'umer','9th','B'),
+(3,'Abdul','11th','c'),
+(4,'umi','9th','B'),
+(5,'rizwan','9th','c')
+
+--Here we update a particular item in a table
+
+update student
+set name ='wasiq'
+where roll_no = 1
+
+--here we delete particular row in table
+delete student where name = 'wasiq'
+
+
